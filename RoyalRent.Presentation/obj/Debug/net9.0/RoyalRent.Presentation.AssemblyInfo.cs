9418bbc987807677e54f12c0d5ebf3b26e82615e
Miguel Pombo <miguel.pomboshape@modec.com>
2025-04-17 14:34:17 -0300

    feat: format only staged files with husky

--- RoyalRent.Presentation/obj/Debug/net9.0/RoyalRent.Presentation.AssemblyInfo.cs
+++ RoyalRent.Presentation/obj/Debug/net9.0/RoyalRent.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoyalRent.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d435bdf78e1fabdc53e21aca12b09bdb193cf88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4f3ded6d4e5c93351840d04005653943b94222")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoyalRent.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoyalRent.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
